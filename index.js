df4072ba07818901d6a3c3a8a454fc84930137d1
pelzfx <111582303+pelzfx@users.noreply.github.com>
2023-03-24 12:49:31 +0100

    added some vanilla js

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function mainContent (){
         <h1>I'm learning reac!</h1>
     )
 }
+
+ReactDom.render(
+    <div>
+        <Navbar />
+        <mainContent />
+    </div>,
+    document.getElementById("root")
+)
+
+// Recreating a code using vanilla javascript
+const h1 = document.createElement("h1");
+h1.textContent = 'This is an imperative way to program'
+h1.className = "header"
+document.getElementById("rot").append(h1)
