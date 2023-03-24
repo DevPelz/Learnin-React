35540a4e9686b07ffc4dc30625e03bb60634a95b
pelzfx <111582303+pelzfx@users.noreply.github.com>
2023-03-24 13:09:56 +0100

    removed the cdn

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function mainContent (){
-    return (
-        <h1>I'm learning reac!</h1>
-    )
-}
+// function mainContent (){
+//     return (
+//         <h1>I'm learning reac!</h1>
+//     )
+// }
 
-ReactDom.render(
-    <div>
-        <Navbar />
-        <mainContent />
-    </div>,
-    document.getElementById("root")
+// ReactDom.render(
+//     <div>
+//         <Navbar />
+//         <mainContent />
+//     </div>,
+//     document.getElementById("root")
+// )
+
+// // Recreating a code using vanilla javascript
+// const h1 = document.createElement("h1");
+// h1.textContent = 'This is an imperative way to program'
+// h1.className = "header"
+// console.log(h1)
+
+// const element = <h1 className="header">This is JSX</h1>
+// console.log(element)
+
+const navbar = (
+    <nav>
+        <h1>Pelz'S Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
-
-// Recreating a code using vanilla javascript
-const h1 = document.createElement("h1");
-h1.textContent = 'This is an imperative way to program'
-h1.className = "header"
-document.getElementById("rot").append(h1)
+// JSX
+ReactDOM.render(navbar, document.getElementById("root"))
