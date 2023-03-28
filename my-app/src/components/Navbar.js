e460eeca3eae21b63a4fafb5f71e7bedce4af6b8
pelzfx <111582303+pelzfx@users.noreply.github.com>
2023-03-28 11:47:05 +0100

    created a static page

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <img src = "../images/react-1-logo.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
---
+++ my-app/src/index.js
@@ -0,0 +1,56 @@
+
+
+// function mainContent (){
+//     return (
+//         <h1>I'm learning reac!</h1>
+//     )
+// }
+
+// ReactDom.render(
+//     <div>
+//         <Navbar />
+//         <mainContent />
+//     </div>,
+//     document.getElementById("root")
+// )
+
+// // Recreating a code using vanilla javascript
+// const h1 = document.createElement("h1");
+// h1.textContent = 'This is an imperative way to program'
+// h1.className = "header"
+// console.log(h1)
+
+// const element = <h1 className="header">This is JSX</h1>
+// console.log(element)
+
+// const page = (
+//     <div>
+//         <h1>Fun facts about React </h1>
+//         <ul>
+//             <li>Was first released in 2013</li>
+//             <li>Was originally created by Jordan Walke</li>
+//             <li>Has well over 100K stars on GitHub</li>
+//             <li>Is maintained by Facebook</li>
+//             <li>Powers thousands of enterprise apps, including mobile apps</li>
+//         </ul>
+//     </div>
+// )
+// // JSX
+// ReactDOM.render(page, document.getElementById("root"));
+
+// Custom Components
+
+
+
+// ReactDOM.render(<TemporaryName />, document.getElementById("root"));
+import React from "react"
+import  ReactDOM  from "react-dom";
+import App from "./app.js";
+import "./style.css"
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root")); 
